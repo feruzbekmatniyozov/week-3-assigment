9c3e515de1a44ab8ba33eecce92bef777d48d9e5
Feruzbek Matniyazov <ferko07uzb@gmail.com>
2025-10-15 10:40:50 +0500

    Week 3 Assignment

---
+++ museum_admission_calculator.py
@@ -0,0 +1,31 @@
+general = 9
+special = 14
+vip = 20
+counter = 0
+subtotal = 0
+discount_amount = 8
+print("=== Museum Admission Calculator ===")
+print("Enter exhibit type: general, special, or vip")
+print("Type 'done' when finished selecting exhibits\n")
+
+
+
+while True:
+    exhibit = input("Enter exhibit type: ")
+    if exhibit == "done":
+        print("=== Admission Summary ===")
+        print(f"Subtotal: ${subtotal:.2f}")
+        if subtotal >= 55:
+            discounted_total = subtotal-discount_amount
+            print(f"Group visit discount: -${discount_amount:.2f}")
+            print(f"Final total: ${discounted_total:.2f}")
+        else:
+            print("Group visit discount: N/A")
+            print(f"Final total: ${subtotal:.2f}")            
+        break
+    elif exhibit == 'general':
+        subtotal = subtotal + general
+    elif exhibit == 'special':
+        subtotal = subtotal + special
+    elif exhibit == 'vip':
+        subtotal = subtotal + vip
